26e9b111ee0e6cd711f85b262e3ea2707c4efda6
Matt J Ashworth <MattJAshworth@users.noreply.github.com>
2020-10-14 01:46:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-// PRESS THE MENU BUTTON TO TRIGGER ANIMATION
-// PRESS PLAY BUTTON TO LISTEN THE DEMO SONG
-
-// As seen on: "https://dribbble.com/shots/2144866-Day-5-Music-Player-Rebound/"
-
-// THANK YOU!
-
 var audio = document.getElementById('audio');
 var playpause = document.getElementById("play");
 
 var trackList = ["londoncalling.mp3", "letsmakeit.mp3"];
 var trackListName = ["London Calling", "Lets Make It"];
 var current = 0;
+
+var timer;
+var percent = 0;
+
+document.getElementById("audio");
+audio.addEventListener("playing", function(_event) {
+  var duration = _event.target.duration;
+  advance(duration, audio);
+});
+audio.addEventListener("pause", function(_event) {
+  clearTimeout(timer);
+});
+var advance = function(duration, element) {
+  var progress = document.getElementById("progress");
+  increment = 10/duration
+  percent = Math.min(increment * element.currentTime * 10, 100);
+  progress.style.width = percent+'%'
+  startTimer(duration, element);
+}
+var startTimer = function(duration, element){ 
+  if(percent < 100) {
+    timer = setTimeout(function (){advance(duration, element)}, 100);
+  }
+}
 
 function togglePlayPause() {
    if (audio.paused || audio.ended) {
